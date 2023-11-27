f40d45e463decfb5cf7fd2480ff3da4063d253d8
Foyez-Ahamed <foyez1435@gmail.com>
2023-11-27 19:41:26 +0600

    install jwt for secure api and also get reviews and categories data from database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -32,15 +33,33 @@ async function run() {
     
     // all collections //
     const userCollections = client.db('kingGalleryDB').collection('users');
+
     const categoriesCollection = client.db('kingGalleryDB').collection('categories');
+
+    const reviewsCollection = client.db('kingGalleryDB').collection('reviews');
+
+    const shopCollections = client.db('kingGalleryDB').collection('shops');
     // all collections //
 
     // curd operation //
 
+    // jwt related api //
+    app.post('/api/v1/jwt', async(req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '4h'});
+      res.send({token});
+    })
+    // jwt related api //
+
     // user related api //
     app.post('/api/v1/createUser', async(req, res) => {
-        const user = req.body;
-        const result = await userCollections.insertOne(user);
+        const users = req.body;
+        const query = {email : users.email};
+        const existingUser = await userCollections.findOne(query);
+        if(existingUser){
+        return res.send({message : 'user already exist', insertedId : null});
+        }
+        const result = await userCollections.insertOne(users);
         res.send(result);
     })
     // user related api //
@@ -52,6 +71,97 @@ async function run() {
       res.send(result);
     })
     // categories related api //
+
+    //  reviews related api //
+    app.get('/api/v1/getReviews', async(req, res) => {
+      const reviews = reviewsCollection.find();
+      const result = await reviews.toArray();
+      res.send(result);
+    })
+    //  reviews related api //
+
+    // shop create related api //
+    app.post('/api/v1/createShop', async(req, res) => {
+      const shopInfo = req.body;
+
+      shopInfo.limit = 3;
+
+      const query = { ownerEmail : shopInfo.ownerEmail};
+
+      const existingShop = await shopCollections.findOne(query);
+
+      if(existingShop) {
+        return res.send({message : 'You have already create a shop', insertedId : null});
+      }
+
+      const result = await shopCollections.insertOne(shopInfo);
+      res.send(result);
+
+    })
+
+    // updated user info //
+    app.patch('/api/v1/updateUserInfo/:email', async(req, res) => {
+      const updateUserInfo = req.body;
+      const email = req.params.email
+      const filter = {email : email}
+
+      const updateUser = {
+        $set : {
+          role : updateUserInfo.role,
+          shopName : updateUserInfo.shopName,
+          shopLogo : updateUserInfo.shopLogo,
+          shopId : updateUserInfo.shopId
+        }
+      }
+
+      const result = await userCollections.updateOne(filter, updateUser);
+
+      res.send(result);
+
+    })
+    // updated user info //
+
+    // get user admin data //
+    app.get('/api/v1/user/admin/:email', async(req, res) => {
+      const email = req.params.email;
+
+      // if(email !== req.decoded.email){
+      //   return res.status(403).send({message: 'forbidden access'})
+      // }
+
+      const query = {email : email}
+      const user = await userCollections.findOne(query)
+
+      let admin = false;
+
+      if(user) {
+        admin = user?.role === 'admin'
+      }
+      res.send({admin});
+    })
+    // get user admin data //
+
+    // get user manager data //
+    app.get('/api/v1/user/manager/:email', async(req, res) => {
+      const email = req.params.email;
+
+      // if(email !== req.decoded.email){
+      //   return res.status(403).send({message: 'forbidden access'})
+      // }
+
+      const query = {email : email}
+      const user = await userCollections.findOne(query)
+
+      let manager = false;
+
+      if(user) {
+        manager = user?.role === 'manager'
+      }
+      res.send({manager});
+    })
+    // get user manager data //
+
+    // shop create related api //
 
     // curd operation //
 
