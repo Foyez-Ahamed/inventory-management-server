e406f27491088cd70dfb12833796ae859f6538ee
Foyez-Ahamed <foyez1435@gmail.com>
2023-11-30 21:04:33 +0600

    some bug fix

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
 
-    await client.connect();
+    // await client.connect();
 
     // all collections //
     const userCollections = client.db("kingGalleryDB").collection("users");
@@ -469,10 +469,10 @@ async function run() {
     // curd operation //
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
